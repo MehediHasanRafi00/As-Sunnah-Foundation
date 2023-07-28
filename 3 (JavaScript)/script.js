641b2b0ef6d56d6d8633d6f05a2457202fdb6c92
Mehedi Hasan Rafi <mehedihasanrafi205@gmail.com>
2023-07-28 12:33:59 +0600

    first commit

---
+++ 3 (JavaScript)/script.js
@@ -0,0 +1,94 @@
+    var day = 'tjhytfj'
+
+    switch(day){
+        case'Sarturday':
+        console.log('This is saturday');
+        break;
+
+        case'Sunday':
+        console.log('This is Holiday');
+        break;
+
+        default:
+            console.log('this is my default text ');
+    }
+
+
+    for(var i = 0; i < 10; i++) {
+        console.log(i + ' My another text' + day);
+    }
+
+
+    var number = 1;
+
+
+
+    do {
+        console.log(number);
+        number++
+    }while(number < 10);
+
+    while(number < 10){
+        console.log(number);
+        number++
+    }
+
+    var name1 = 'kabul'
+    var name2 = 'ryhan'
+    var name3 = 'rafi'
+
+
+    var aNeme = [
+        'kabul',
+        'ryhan',
+        7,
+        'BD'
+    ] 
+
+    aNeme.push ("Test")  
+    aNeme.shift ()  
+
+    console.log(aName[1])
+
+    for( var i = 0; i < aNeme.length; i++) {
+        console.log(aNeme[i])
+    }
+
+
+
+
+console.log(aNeme.indexOf('rafi'))
+
+
+    aNeme.splice(1,2,3)
+
+    console.log(aNeme)
+
+    info = {
+        name: Rashed,
+        age: 30,
+        adress: Test
+    }
+
+
+    info.age
+
+
+    var info = {
+        fullName: 'Rasel Ahmed',
+        age: 30,
+        job: 'web dev',
+        hello: alert("Hello Friend")
+    }
+
+    info.hello
+
+    var a = 10;
+    var b = 20;
+
+function sumMechine( a,b){
+    var sum = a + b;
+    return sum;
+}
+
+console.log(sumMechine(20 , 40));
